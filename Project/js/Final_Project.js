a09340d0bc6289d0781f2d3ac015e06047325b4e
JJFRENZY <jackjayeck@icloud.com>
2024-10-25 22:56:50 -0600

    week06

--- Project/js/Final_Project.js
+++ Project/js/Final_Project.js
@@ -5,7 +5,7 @@ function displayDeckContent(deck) {
     if (deck === 'kaito-pair') {
         content = `
         <aside>
-            <h2>Evil★Twin the Kaito Pair</h2>
+            <h2 style="color: #ff00b7;">Evil★Twin the Kaito Pair</h2>
         </aside>
         <main>
             <section id="hero">
@@ -80,88 +80,84 @@ function displayDeckContent(deck) {
     } else if (deck === 'eldlich-conqueror') {
         content = `
         <aside>
-        <h2>Eldlich the Conqueror</h2>
-    </aside>
-    <main>
-        <section id="hero">
-            <div class="hero-content">
-                <img src="images/TTD_EC_DB.png" alt="Eldlich Deck Box">
-                <div class="deck-info">
-                    <h2>Main Deck</h2>
-                    <h3>Monsters</h3>
-                    <ul>
-                        <li>3x Eldlich the Golden Lord</li>
-                        <li>Doomking Balderoch</li>
-                        <li>Necroworld Banshee</li>
-                        <li>2x Ash Blossom & Joyous Spring</li>
-                    </ul>
-                    <h3>Spells</h3>
-                    <ul>
-                        <li>2x Cursed Eldland</li>
-                        <li>3x Eldlixir of Black Awakening</li>
-                        <li>Eldlixir of White Destiny</li>
-                        <li>Zombie World</li>
-                        <li>Pot of Extravagance</li>
-                        <li>Pot of Prosperity</li>
-                        <li>Foolish Burial</li>
-                        <li>Foolish Burial Goods</li>
-                        <li>Lightning Storm</li>
-                        <li>Cosmic Cyclone</li>
-                    </ul>
-                    <h3>Traps</h3>
-                    <ul>
-                        <li>3x Eldlixir of Scarlet Sanguine</li>
-                        <li>2x Conquistador of the Golden Land</li>
-                        <li>2x Huaquero of the Golden Land</li>
-                        <li>Golden Land Forever!</li>
-                        <li>Solemn Judgment</li>
-                        <li>Skill Drain</li>
-                        <li>Rivalry of Warlords</li>
-                        <li>Gozen Match</li>
-                        <li>There Can Be Only One</li>
-                        <li>Dogmatika Punishment</li>
-                        <li>2x Torrential Tribute</li>
-                        <li>2x Infinite Impermanence</li>
-                    </ul>
-                    <h3>Extra Deck</h3>
-                    <ul>
-                        <li>Elder Entity N'tss</li>
-                        <li>Garura, Wings of Resonant Life</li>
-                        <li>Constellar Pleiades</li>
-                        <li>Superdreadnought Rail Cannon Gustav Max</li>
-                        <li>Number 81: Superdreadnought Rail Cannon Super Dora</li>
-                        <li>Superdreadnought Rail Cannon Juggernaut Liebe</li>
-                        <li>Divine Arsenal AA-Zeus - Sky Thunder</li>
-                        <li>Link Spider</li>
-                        <li>Vampire Sucker</li>
-                        <li>Knightmare Phoenix</li>
-                    </ul>
-                </div>
-            </div>
-        </section>
-        <aside>
-            <h2>Highlight Cards</h2>
+            <h2 style="color: #FFD700;">Eldlich the Conqueror</h2>
         </aside>
-        <section id="features">
-            <div class="feature">
-                <h3><img src="images/TTD_EC_Cursed_Eldland.png" alt="Cursed Eldland"></h3>
-
-            </div>
-            <div class="feature">
-                <h3><img src="images/TTD_EC_Eldlich_the_Golden_Lord.png" alt="Eldlich the Golden Lord"></h3>
-
-            </div>
-            <div class="feature">
-                <h3><img src="images/TTD_EC_Eldlixir_of_Scarlet_Sanguine.png" alt="Eldlixir of Scarlet Sanguine"></h3>
-
-            </div>
-        </section>
-    </main>
-</body>
-</html>
-`;
+        <main>
+            <section id="hero">
+                <div class="hero-content">
+                    <img src="images/TTD_EC_DB.png" alt="Eldlich Deck Box">
+                    <div class="deck-info">
+                        <h2>Main Deck</h2>
+                        <h3>Monsters</h3>
+                        <ul>
+                            <li>3x Eldlich the Golden Lord</li>
+                            <li>Doomking Balderoch</li>
+                            <li>Necroworld Banshee</li>
+                            <li>2x Ash Blossom & Joyous Spring</li>
+                        </ul>
+                        <h3>Spells</h3>
+                        <ul>
+                            <li>2x Cursed Eldland</li>
+                            <li>3x Eldlixir of Black Awakening</li>
+                            <li>Eldlixir of White Destiny</li>
+                            <li>Zombie World</li>
+                            <li>Pot of Extravagance</li>
+                            <li>Pot of Prosperity</li>
+                            <li>Foolish Burial</li>
+                            <li>Foolish Burial Goods</li>
+                            <li>Lightning Storm</li>
+                            <li>Cosmic Cyclone</li>
+                        </ul>
+                        <h3>Traps</h3>
+                        <ul>
+                            <li>3x Eldlixir of Scarlet Sanguine</li>
+                            <li>2x Conquistador of the Golden Land</li>
+                            <li>2x Huaquero of the Golden Land</li>
+                            <li>Golden Land Forever!</li>
+                            <li>Solemn Judgment</li>
+                            <li>Skill Drain</li>
+                            <li>Rivalry of Warlords</li>
+                            <li>Gozen Match</li>
+                            <li>There Can Be Only One</li>
+                            <li>Dogmatika Punishment</li>
+                            <li>2x Torrential Tribute</li>
+                            <li>2x Infinite Impermanence</li>
+                        </ul>
+                        <h3>Extra Deck</h3>
+                        <ul>
+                            <li>Elder Entity N'tss</li>
+                            <li>Garura, Wings of Resonant Life</li>
+                            <li>Constellar Pleiades</li>
+                            <li>Superdreadnought Rail Cannon Gustav Max</li>
+                            <li>Number 81: Superdreadnought Rail Cannon Super Dora</li>
+                            <li>Superdreadnought Rail Cannon Juggernaut Liebe</li>
+                            <li>Divine Arsenal AA-Zeus - Sky Thunder</li>
+                            <li>Link Spider</li>
+                            <li>Vampire Sucker</li>
+                            <li>Knightmare Phoenix</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <aside>
+                <h2>Highlight Cards</h2>
+            </aside>
+            <section id="features">
+                <div class="feature">
+                    <h3><img src="images/TTD_EC_Cursed_Eldland.png" alt="Cursed Eldland"></h3>
+                </div>
+                <div class="feature">
+                    <h3><img src="images/TTD_EC_Eldlich_the_Golden_Lord.png" alt="Eldlich the Golden Lord"></h3>
+                </div>
+                <div class="feature">
+                    <h3><img src="images/TTD_EC_Eldlixir_of_Scarlet_Sanguine.png" alt="Eldlixir of Scarlet Sanguine"></h3>
+                </div>
+            </section>
+        </main>`;
     } else if (deck === 'cyber-dragon') {
         content = `
+        <aside>
+            <h2 style="color: #C0C0C0;">Cyber Dragon</h2>
         </aside>
         <main>
             <section id="hero">
@@ -190,32 +186,32 @@ function displayDeckContent(deck) {
                             <li>Cyberdark World</li>
                             <li>Machine Duplication</li>
                             <li>2x Clockwork Night</li>
-                            <li>Triple Tactic Talents</li>
-                            <li>Triple Tactic Thrust</li>
+                            <li>Triple Tactics Talent</li>
+                            <li>Triple Tactics Thrust</li>
                             <li>Sales Ban</li>
                             <li>Raigeki</li>
                             <li>Harpie's Feather Duster</li>
                             <li>2x Lightning Storm</li>
                             <li>2x Called by the Grave</li>
-                            <li>Forbidden Droplet</li>
                         </ul>
                         <h3>Traps</h3>
                         <ul>
-                            <li>Red Reboot</li>
-                            <li>Infinite Impermanence</li>
+                            <li>3x Infinite Impermanence</li>
+                            <li>3x Cybernetic Overflow</li>
                         </ul>
                         <h3>Extra Deck</h3>
                         <ul>
-                            <li>Cyber End Dragon</li>
+                            <li>2x Chimeratech Mega Fleet Dragon</li>
+                            <li>2x Chimeratech Rampage Dragon</li>
                             <li>Cyber Twin Dragon</li>
-                            <li>Chimeratech Megafleet Dragon</li>
-                            <li>Chimeratech Fortress Dragon</li>
-                            <li>Chimeratech Rampage Dragon</li>
-                            <li>Cyber Dragon Nova</li>
-                            <li>Cyber Dragon Infinity</li>
-                            <li>Cyber Dragon Sieger</li>
-                            <li>Sky Striker Ace - Azalea</li>
-                            <li>Salamangreat Almiraj</li>
+                            <li>Cyber Twin Dragon</li>
+                            <li>Cyber Twin Dragon</li>
+                            <li>Cyber Twin Dragon</li>
+                            <li>Cyber Twin Dragon</li>
+                            <li>Cyber Twin Dragon</li>
+                            <li>Cyber Twin Dragon</li>
+                            <li>Cyber Twin Dragon</li>
+                            <li>Cyber Twin Dragon</li>
                         </ul>
                     </div>
                 </div>
@@ -225,40 +221,26 @@ function displayDeckContent(deck) {
             </aside>
             <section id="features">
                 <div class="feature">
-                    <h3><img src="images/TTD_CD_Power_Bond.png" alt="Power_Bond"></h3>
-    
+                    <h3><img src="images/TTD_CD_Cyber_Dragon.png" alt="Cyber Dragon"></h3>
                 </div>
                 <div class="feature">
-                    <h3><img src="images/TTD_CD_Cyber_End_Dragon.png" alt="Cyber End Dragon"></h3>
-    
+                    <h3><img src="images/TTD_CD_Cyber_Dragon_Core.png" alt="Cyber Dragon Core"></h3>
                 </div>
                 <div class="feature">
-                    <h3><img src="images/TTD_CD_Infinite_Impermanence.png" alt="Infinite Impermanence"></h3>
-    
+                    <h3><img src="images/TTD_CD_Power_Bond.png" alt="Power Bond"></h3>
                 </div>
             </section>
-        </main>
-    </body>
-    </html>
-    `;
+        </main>`;
     }
 
-    // Update the main content area with the selected deck's content
-    document.getElementById('content').innerHTML = content;
+    const contentContainer = document.getElementById('deck-content');
+    contentContainer.innerHTML = content;
 }
 
-// Event listeners for each deck link
-document.getElementById('kaito-pair').addEventListener('click', function(e) {
-    e.preventDefault();
-    displayDeckContent('kaito-pair');
-});
-
-document.getElementById('eldlich-conqueror').addEventListener('click', function(e) {
-    e.preventDefault();
-    displayDeckContent('eldlich-conqueror');
-});
-
-document.getElementById('cyber-dragon').addEventListener('click', function(e) {
-    e.preventDefault();
-    displayDeckContent('cyber-dragon');
+// Event listeners for deck links
+document.querySelectorAll('.deck-link').forEach(link => {
+    link.addEventListener('click', (event) => {
+        const deckType = event.target.getAttribute('data-deck');
+        displayDeckContent(deckType);
+    });
 });
